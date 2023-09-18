3435bd57d7778406ce3906f31362b765f950e8f7
DaylinS <144367590+DaylinS@users.noreply.github.com>
2023-09-18 08:57:10 -0700

    Add project files.

---
+++ Project1/CodeFile1.cs
@@ -0,0 +1,18 @@
+﻿/*
+Daylin McDaniel
+C++ semester 1 2023
+Due: 9-20-23
+Lab 1 Hello World
+Create a code to say hello world*/
+
+#include <iostream>
+#include <windows.h>
+
+int main()
+{
+    system("color 0B")
+
+    std::cout << "Hello World";
+
+    return 0;
+}
